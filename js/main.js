6df7bb2af9160629d840e67ed0a837d0c6d28abb
200103020 <200103020@stu.sdu.edu.kz>
2024-04-08 14:48:17 +0500

    test

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.review_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        dots: false,
+        arrows: true,
+        fade: !0,
+        cssEase: 'ease',
+    });
+
+});
